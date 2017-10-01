a46dbc2eda3ef8933408d9169c5d101e6e7468f8
evpozdniakov <evpozdniakov@gmail.com>
2017-10-01 19:02:56 -0400

    Use MS Translator

--- server.js
+++ server.js
@@ -15,6 +15,13 @@ app.use(require('webpack-dev-middleware')(compiler, {
 
 app.use(require('webpack-hot-middleware')(compiler));
 
+app.get('/translate/:text', function (req, res) {
+  translateWithAPI(req.params.text)
+    .then(jsonString => {
+      res.send(jsonString);
+    });
+});
+
 app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
@@ -26,3 +33,59 @@ app.listen(3000, function(err) {
 
   console.log('Listening at http://localhost:3000/');
 });
+
+function translateWithAPI(text) {
+  const fetch = require('node-fetch');
+  const { url, options } = prepareTranslateRequest(text);
+
+  return fetch(url, options)
+    .then(res => res.text())
+    .then(parseXml)
+    .then(json => {
+      const translation = json.string._;
+
+      return JSON.stringify({translation});
+    });
+}
+
+function prepareTranslateRequest(text) {
+  const params = {
+    appid: '',
+    text,
+    from: 'en',
+    to: 'ru',
+    contentType: 'text/plain',
+    category: 'general',
+  };
+
+  const url = 'https://api.microsofttranslator.com/V2/Http.svc/Translate?' + toQueryString(params);
+  const { API_KEY } = require('./api-key');
+
+  const options = {
+    method: 'GET',
+    headers: {
+      'Ocp-Apim-Subscription-Key': API_KEY.MICROSOFT_TRANSLATOR,
+    },
+  };
+
+  return {url, options};
+}
+
+function toQueryString(obj) {
+  return Object.keys(obj)
+    .map(key => `${key}=${encodeURIComponent(obj[key])}`)
+    .join('&');
+}
+
+function parseXml(xml) {
+  return new Promise((resolve, reject) => {
+    require('xml2js').parseString(xml, (err, result) => {
+      if (err) {
+        reject(err);
+      }
+      else {
+        resolve(result);
+      }
+    });
+  });
+}
--- src/ReaderUI/middlewares/reader.js
+++ src/ReaderUI/middlewares/reader.js
@@ -24,20 +24,15 @@ export function handleTranslateText(store, next, action) {
   })
 
   const text = encodeURIComponent(data.text)
-  const url = 'https://translation.googleapis.com/language/translate/v2'
-  + `?key=${API_KEY.GOOGLE_TRANSLATE}`
-  + `&source=en&target=ru&q=${text}&format=text&model=nmt`
+  const url = `/translate/${text}`
 
   fetch(url)
     .then(res => res.json())
     .then(res => {
-      const { translations=[] } = res.data || {}
-      const { translatedText } = translations[0] || {}
-
       next({
         ...action,
         type: type + _DONE,
-        response: translatedText,
+        response: res.translation,
       })
     })
     .catch(error => {
